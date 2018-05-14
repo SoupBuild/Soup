7d29a8fbcc041db8f589c35a7fbb6070dac8f6e9
Matthew Asplund <mwasplund@outlook.com>
2018-05-13 20:49:54 -0700

    Add Make for linux

---
+++ Source/Make/BuildRunner.cs
@@ -0,0 +1,16 @@
+﻿// <copyright company="Soup" file="BuildRunner.cs">
+//   Copyright (c) Soup.  All rights reserved.
+// </copyright>
+
+namespace Soup.Make
+{
+	using System;
+
+	public class BuildRunner : IBuildRunner
+	{
+		public bool Build(string path, bool showOutput, bool debug)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
