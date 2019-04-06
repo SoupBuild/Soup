e5cc5d661c36ba06b4e017f8fccd536c1bc5e1ed
Matthew Asplund <mwasplund@outlook.com>
2019-04-06 14:50:13 -0700

    Move main exe recipe into the client project

---
+++ Source/Client/Main.cpp
@@ -0,0 +1,10 @@
+﻿// <copyright file="Main.cpp" company="Soup">
+// Copyright (c) Soup. All rights reserved.
+// </copyright>
+
+import std.core;
+
+int main()
+{
+    std::cout << "Running..." << std::endl;
+}
