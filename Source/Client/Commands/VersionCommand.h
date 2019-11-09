d6bb4e60b415dfefe893cc72772b27d00f8df692
Matthew Asplund <mwasplund@outlook.com>
2019-11-09 15:53:23 -0800

    Fix log for version

--- Source/Client/Commands/VersionCommand.h
+++ Source/Client/Commands/VersionCommand.h
@@ -31,7 +31,7 @@ namespace Soup::Client
 
 			// TODO var version = Assembly.GetExecutingAssembly().GetName().Version;
 			// Log::Message($"{version.Major}.{version.Minor}.{version.Build}");
-			Log::Info("0.2.10");
+			Log::HighPriority("0.2.10");
 		}
 
 	private:
