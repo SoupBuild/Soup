0996f24c178fb6a77dc62a873288a9203ca75582
Matthew Asplund <mwasplund@outlook.com>
2020-01-20 10:21:51 -0800

    Add toml as a copy of json version

---
+++ Source/Core/Package/RecipeToml.h
@@ -0,0 +1,48 @@
+﻿// <copyright file="RecipeToml.h" company="Soup">
+// Copyright (c) Soup. All rights reserved.
+// </copyright>
+
+#pragma once
+#include "Recipe.h"
+
+namespace Soup
+{
+	/// <summary>
+	/// The recipe Toml serialize manager
+	/// </summary>
+	export class RecipeToml
+	{
+	public:
+		/// <summary>
+		/// Load from stream
+		/// </summary>
+		static Recipe Deserialize(std::istream& stream)
+		{
+			// Read the entire file into a string
+			std::string content(
+				(std::istreambuf_iterator<char>(stream)),
+				std::istreambuf_iterator<char>());
+
+			// Read the contents of the recipe file
+			auto root = toml::parse(content);
+			auto& rootTable = root.as_table();
+			for (auto& element : rootTable)
+			{
+				std::cout << element->first << std::endl;
+			}
+		}
+
+		/// <summary>
+		/// Save the recipe to the root file
+		/// </summary>
+		static void Serialize(const Recipe& recipe, std::ostream& stream)
+		{
+			// Serialize the contents of the recipe
+			json11::Json json = BuildJson(recipe);
+
+			stream << json.dump();
+		}
+
+	private:
+	};
+}
