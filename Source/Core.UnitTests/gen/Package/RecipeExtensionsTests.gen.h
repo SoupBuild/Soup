ab1e36011e594fbdc12008acc241c652d5ba7d22
Matthew Asplund <mwasplund@outlook.com>
2020-02-01 20:13:00 -0800

    Enable serialize

--- Source/Core.UnitTests/gen/Package/RecipeExtensionsTests.gen.h
+++ Source/Core.UnitTests/gen/Package/RecipeExtensionsTests.gen.h
@@ -9,7 +9,7 @@ TestState RunRecipeExtensionsTests()
 	state += SoupTest::RunTest(className, "TryLoadFromFile_MissingFile", [&testClass]() { testClass->TryLoadFromFile_MissingFile(); });
 	state += SoupTest::RunTest(className, "TryLoadFromFile_GarbageFile", [&testClass]() { testClass->TryLoadFromFile_GarbageFile(); });
 	state += SoupTest::RunTest(className, "TryLoadFromFile_SimpleFile", [&testClass]() { testClass->TryLoadFromFile_SimpleFile(); });
-	// state += SoupTest::RunTest(className, "SaveToFile_SimpleFile", [&testClass]() { testClass->SaveToFile_SimpleFile(); });
+	state += SoupTest::RunTest(className, "SaveToFile_SimpleFile", [&testClass]() { testClass->SaveToFile_SimpleFile(); });
 	state += SoupTest::RunTest(className, "GetRecipeOutputPath_MissingRecipeThrows", [&testClass]() { testClass->GetRecipeOutputPath_MissingRecipeThrows(); });
 	state += SoupTest::RunTest(className, "GetRecipeOutputPath_Exists", [&testClass]() { testClass->GetRecipeOutputPath_Exists(); });
 	state += SoupTest::RunTest(className, "GetPackageReferencePath_IsRooted", [&testClass]() { testClass->GetPackageReferencePath_IsRooted(); });
