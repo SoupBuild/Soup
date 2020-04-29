0398173d1daecce1d1d45b092a8e7d119c5fd62a
Matthew Asplund <mwasplund@outlook.com>
2020-04-29 11:42:29 -0700

    Add build order checks

--- Source/Build/Runtime/BuildSystem.h
+++ Source/Build/Runtime/BuildSystem.h
@@ -7,6 +7,27 @@
 
 namespace Soup::Build::Runtime
 {
+	class BuildTaskContainer
+	{
+	public:
+		BuildTaskContainer(
+			Memory::Reference<IBuildTask> task,
+			std::vector<std::string> runBeforeList,
+			std::vector<std::string> runAfterList) :
+			Task(std::move(task)),
+			RunBeforeList(std::move(runBeforeList)),
+			RunAfterList(std::move(runAfterList)),
+			HasRun(false)
+		{
+		}
+
+		Memory::Reference<IBuildTask> Task;
+		std::vector<std::string> RunBeforeList;
+		std::vector<std::string> RunAfterList;
+		std::vector<std::string> RunAfterClosureList;
+		bool HasRun;
+	};
+
 	/// <summary>
 	/// The build system implementation
 	/// </summary>
@@ -33,10 +54,15 @@ namespace Soup::Build::Runtime
 				std::string taskName = ourTask->GetName();
 				Log::Diag("RegisterTask: " + taskName);
 
+				auto taskContainer = BuildTaskContainer(
+					ourTask,
+					Extensions::StringListWrapper(ourTask->GetRunBeforeList()).CopyAsStringVector(),
+					Extensions::StringListWrapper(ourTask->GetRunAfterList()).CopyAsStringVector());
+
 				std::stringstream runBeforeMessage;
 				runBeforeMessage << "RunBefore [";
 				bool isFirst = true;
-				for (auto& name : Extensions::StringListWrapper(ourTask->GetRunBeforeList()).CopyAsStringVector())
+				for (auto& name : taskContainer.RunBeforeList)
 				{
 					if (!isFirst)
 						runBeforeMessage << ", ";
@@ -51,7 +77,7 @@ namespace Soup::Build::Runtime
 				std::stringstream runAfterMessage;
 				runAfterMessage << "RunAfter [";
 				isFirst = true;
-				for (auto& name : Extensions::StringListWrapper(ourTask->GetRunAfterList()).CopyAsStringVector())
+				for (auto& name : taskContainer.RunAfterList)
 				{
 					if (!isFirst)
 						runAfterMessage << ", ";
@@ -63,7 +89,7 @@ namespace Soup::Build::Runtime
 				runAfterMessage << "]";
 				Log::Diag(runAfterMessage.str());
 
-				auto insertResult = _tasks.try_emplace(taskName, ourTask);
+				auto insertResult = _tasks.try_emplace(taskName, std::move(taskContainer));
 				if (!insertResult.second)
 				{
 					Log::HighPriority("A task with the provided name has already been registered: " + taskName);
@@ -84,24 +110,102 @@ namespace Soup::Build::Runtime
 		/// </summary>
 		void Execute(BuildState& state)
 		{
+			// Setup each task to have a complete list of tasks that must run before itself
+			// Note: this is required to combine other tasks run before lists with the tasks
+			// own run after list
 			for (auto& task : _tasks)
 			{
-				Log::Info("TaskStart: " + task.first);
-				auto status = task.second->Execute(state);
+				auto& taskContainer = task.second;
+
+				// Copy their own run after list
+				std::copy(
+					taskContainer.RunAfterList.begin(),
+					taskContainer.RunAfterList.end(),
+					std::back_inserter(taskContainer.RunAfterClosureList));
+
+				// Add ourself to all tasks in our run before list
+				for (auto& runBefore : taskContainer.RunBeforeList)
+				{
+					// Try to find the other task
+					auto findResult = _tasks.find(runBefore);
+					if (findResult != _tasks.end())
+					{
+						auto& beforeTaskContainer = findResult->second;
+						beforeTaskContainer.RunAfterClosureList.push_back(task.first);
+					}
+				}
+			}
+
+			// Run all tasks in the order they were registered
+			// ensuring they are run in the correct dependency order
+			auto currentTask = FindNextTask();
+			while (currentTask != _tasks.end())
+			{
+				Log::Info("TaskStart: " + currentTask->first);
+				auto status = currentTask->second.Task->Execute(state);
 				if (status != 0)
 				{
 					Log::Error("TaskFailed: " + std::to_string(status));
+					throw std::runtime_error("Task Failed");
 				}
 				else
 				{
-					Log::Info("TaskDone: " + task.first);
+					Log::Info("TaskDone: " + currentTask->first);
 				}
 
 				state.LogActive();
+				currentTask->second.HasRun = true;
+				currentTask = FindNextTask(); 
 			}
 		}
 
 	private:
-		std::unordered_map<std::string, Memory::Reference<IBuildTask>> _tasks;
+		/// <summary>
+		/// Try to find the next task that has yet to be run and is ready
+		/// Returns false if all tasks have been run
+		/// Throws error if we hit a deadlock
+		/// </summary>
+		std::unordered_map<std::string, BuildTaskContainer>::iterator FindNextTask()
+		{
+			// Find the next task that is ready to be run
+			bool hasAnyStillWaiting = false;
+			auto taskItr = _tasks.begin();
+			for (;taskItr != _tasks.end(); taskItr++)
+			{
+				// Check if this task has run already, 
+				// if not check if all if all upstream tasks have finished
+				auto& taskContainer = taskItr->second;
+				if (!taskContainer.HasRun)
+				{
+					hasAnyStillWaiting = true;
+
+					// Check if all of their run after dependencies have already finished
+					bool hasDependencyPending = false;
+					for (auto& runBefore : taskContainer.RunAfterClosureList)
+					{
+						auto findResult = _tasks.find(runBefore);
+						if (findResult != _tasks.end() && !findResult->second.HasRun)
+						{
+							// Found a dependency that hasn't run, keep trying
+							hasDependencyPending = true;
+							break;
+						}
+					}
+
+					// All dependencies have finished
+					// Let's run this one
+					if (!hasDependencyPending)
+						return taskItr;
+				}
+			}
+
+			if (!hasAnyStillWaiting)
+				return _tasks.end();
+			else
+				throw std::runtime_error("Hit deadlock in build task dependencies.");
+		}
+
+	private:
+		std::unordered_map<std::string, BuildTaskContainer> _tasks;
 	};
 }
