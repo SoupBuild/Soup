14c1ce91131db3432d3e507e608f788831bf9d78
mwasplund <mwasplund@gmail.com>
2020-08-05 10:19:20 -0700

    Update version

--- Source/Client/CLI/Commands/VersionCommand.h
+++ Source/Client/CLI/Commands/VersionCommand.h
@@ -31,7 +31,7 @@ namespace Soup::Client
 
 			// TODO var version = Assembly.GetExecutingAssembly().GetName().Version;
 			// Log::Message($"{version.Major}.{version.Minor}.{version.Build}");
-			Log::HighPriority("0.8.4");
+			Log::HighPriority("0.8.5");
 		}
 
 	private:
