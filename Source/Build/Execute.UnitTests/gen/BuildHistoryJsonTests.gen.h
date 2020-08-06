ae7c1dca945f7617ae5b5a454254a9f9cf05363b
mwasplund <mwasplund@gmail.com>
2020-08-06 15:30:39 -0700

    Update history json tests

--- Source/Build/Execute.UnitTests/gen/BuildHistoryJsonTests.gen.h
+++ Source/Build/Execute.UnitTests/gen/BuildHistoryJsonTests.gen.h
@@ -7,9 +7,10 @@ TestState RunBuildHistoryJsonTests()
 	auto testClass = std::make_shared<Soup::Build::Execute::UnitTests::BuildHistoryJsonTests>();
 	TestState state = { 0, 0 };
 	state += SoupTest::RunTest(className, "Deserialize_GarbageThrows", [&testClass]() { testClass->Deserialize_GarbageThrows(); });
-	state += SoupTest::RunTest(className, "Deserialize_MissingKnownFiles", [&testClass]() { testClass->Deserialize_MissingKnownFiles(); });
-	state += SoupTest::RunTest(className, "Deserialize_MissingFileThrows", [&testClass]() { testClass->Deserialize_MissingFileThrows(); });
-	state += SoupTest::RunTest(className, "Deserialize_MissingIncludesThrows", [&testClass]() { testClass->Deserialize_MissingIncludesThrows(); });
+	state += SoupTest::RunTest(className, "Deserialize_MissingOperationsThrows", [&testClass]() { testClass->Deserialize_MissingOperations(); });
+	state += SoupTest::RunTest(className, "Deserialize_MissingCommandThrows", [&testClass]() { testClass->Deserialize_MissingCommandThrows(); });
+	state += SoupTest::RunTest(className, "Deserialize_MissingInputThrows", [&testClass]() { testClass->Deserialize_MissingInputThrows(); });
+	state += SoupTest::RunTest(className, "Deserialize_MissingOutputThrows", [&testClass]() { testClass->Deserialize_MissingOutputThrows(); });
 	state += SoupTest::RunTest(className, "Deserialize_Simple", [&testClass]() { testClass->Deserialize_Simple(); });
 	state += SoupTest::RunTest(className, "Deserialize_Multiple", [&testClass]() { testClass->Deserialize_Multiple(); });
 	state += SoupTest::RunTest(className, "Serialize_Simple", [&testClass]() { testClass->Serialize_Simple(); });
