5834f4113ed7cf4499d99a919a1cd8f5222e2761
mwasplund <mwasplund@gmail.com>
2020-08-06 15:37:02 -0700

    Fix build history manager tests

--- Source/Build/Execute.UnitTests/gen/BuildHistoryJsonTests.gen.h
+++ Source/Build/Execute.UnitTests/gen/BuildHistoryJsonTests.gen.h
@@ -7,7 +7,7 @@ TestState RunBuildHistoryJsonTests()
 	auto testClass = std::make_shared<Soup::Build::Execute::UnitTests::BuildHistoryJsonTests>();
 	TestState state = { 0, 0 };
 	state += SoupTest::RunTest(className, "Deserialize_GarbageThrows", [&testClass]() { testClass->Deserialize_GarbageThrows(); });
-	state += SoupTest::RunTest(className, "Deserialize_MissingOperationsThrows", [&testClass]() { testClass->Deserialize_MissingOperations(); });
+	state += SoupTest::RunTest(className, "Deserialize_MissingOperationsThrows", [&testClass]() { testClass->Deserialize_MissingOperationsThrows(); });
 	state += SoupTest::RunTest(className, "Deserialize_MissingCommandThrows", [&testClass]() { testClass->Deserialize_MissingCommandThrows(); });
 	state += SoupTest::RunTest(className, "Deserialize_MissingInputThrows", [&testClass]() { testClass->Deserialize_MissingInputThrows(); });
 	state += SoupTest::RunTest(className, "Deserialize_MissingOutputThrows", [&testClass]() { testClass->Deserialize_MissingOutputThrows(); });
