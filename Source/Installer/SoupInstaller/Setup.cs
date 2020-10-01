1f64b9a64204a34900e95ce6bf7042a3db8dde40
Matthew Asplund <mwasplund@outlook.com>
2020-10-01 16:53:05 -0700

    Bump version

--- Source/Installer/SoupInstaller/Setup.cs
+++ Source/Installer/SoupInstaller/Setup.cs
@@ -27,12 +27,21 @@ class Script
 			});
 
 		project.Platform = Platform.x64;
-		project.Version = new Version(0, 8, 11);
 		project.GUID = new Guid("2CF2A792-A266-416D-ACF9-B81781DC2B76");
 		project.BackgroundImage = @"Images\DialogBackground.bmp";
 		project.BannerImage = @"Images\DialogBanner.bmp";
 		project.LicenceFile = @"License.rtf";
 		project.OutDir = @"msi";
+
+		// Prevent two installations at the same time
+		project.MajorUpgrade = new MajorUpgrade()
+		{
+			Schedule = UpgradeSchedule.afterInstallInitialize,
+			DowngradeErrorMessage = "A later version of [ProductName] is already installed. Setup will now exit.",
+		};
+
+		// Upgrade values
+		project.Version = new Version(0, 9, 0);
 
 		Compiler.BuildMsi(project);
 	}
