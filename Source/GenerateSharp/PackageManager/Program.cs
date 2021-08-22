4f0489aed76282004d2eb9bc4b4cf56b0a19a6a2
Matthew Asplund <mwasplund@outlook.com>
2021-08-22 11:13:38 -0700

    Create package manager

---
+++ Source/GenerateSharp/PackageManager/Program.cs
@@ -0,0 +1,16 @@
+﻿// <copyright file="Program.cs" company="Soup">
+// Copyright (c) Soup. All rights reserved.
+// </copyright>
+
+namespace Soup.Build.PackageManager
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
