aff0c48b91480b25799043b94dfc1b4f05d607dc
Matthew Asplund <mwasplund@outlook.com>
2021-12-31 21:33:12 -0800

    Move build runtime into client core

--- Source/Client/Core/UnitTests/gen/Main.cpp
+++ Source/Client/Core/UnitTests/gen/Main.cpp
@@ -18,6 +18,28 @@ using namespace Opal;
 using namespace Opal::System;
 using namespace Soup::Test;
 
+#include "BuildHistoryCheckerTests.gen.h"
+#include "BuildEvaluateEngineTests.gen.h"
+#include "BuildGenerateEngineTests.gen.h"
+#include "FileSystemStateTests.gen.h"
+
+#include "Contracts/ValueTableManagerTests.gen.h"
+#include "Contracts/ValueTableReaderTests.gen.h"
+#include "Contracts/ValueTableWriterTests.gen.h"
+
+#include "LocalUserConfig/LocalUserConfigExtensionsTests.gen.h"
+#include "LocalUserConfig/LocalUserConfigTests.gen.h"
+
+#include "OperationGraph/OperationGraphTests.gen.h"
+#include "OperationGraph/OperationGraphManagerTests.gen.h"
+#include "OperationGraph/OperationGraphReaderTests.gen.h"
+#include "OperationGraph/OperationGraphWriterTests.gen.h"
+
+#include "Recipe/PackageReferenceTests.gen.h"
+#include "Recipe/RecipeExtensionsTests.gen.h"
+#include "Recipe/RecipeTests.gen.h"
+#include "Recipe/RecipeTomlTests.gen.h"
+
 #include "Package/PackageManagerTests.gen.h"
 #include "Package/RecipeBuilderTests.gen.h"
 #include "Package/RecipeBuildManagerTests.gen.h"
@@ -32,9 +54,27 @@ int main()
 	state += RunRecipeBuilderTests();
 	state += RunRecipeBuildManagerTests();
 
-	// Touch stamp file to ensure incremental builds work
-	// auto testFile = std::fstream("TestHarness.stamp", std::fstream::out);
-	// testFile << "TOUCH";
+	state += RunBuildHistoryCheckerTests();
+	state += RunBuildEvaluateEngineTests();
+	state += RunBuildGenerateEngineTests();
+	state += RunFileSystemStateTests();
+
+	state += RunLocalUserConfigExtensionsTests();
+	state += RunLocalUserConfigTests();
+
+	state += RunValueTableManagerTests();
+	state += RunValueTableReaderTests();
+	state += RunValueTableWriterTests();
+
+	state += RunOperationGraphTests();
+	state += RunOperationGraphManagerTests();
+	state += RunOperationGraphReaderTests();
+	state += RunOperationGraphWriterTests();
+
+	state += RunPackageReferenceTests();
+	state += RunRecipeExtensionsTests();
+	state += RunRecipeTests();
+	state += RuneRecipeTomlTests();
 
 	std::cout << state.PassCount << " PASSED." << std::endl;
 	std::cout << state.FailCount << " FAILED." << std::endl;
