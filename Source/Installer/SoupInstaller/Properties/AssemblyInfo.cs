9a0dd282adf625378dc44dada7758747bb908217
Matthew Asplund <mwasplund@gmail.com>
2022-01-04 00:15:42 -0800

    Add Code Signing (#111)
    * Update OpenSSL
    * Move soup exe to sub folder
    * Add signing scripts

--- Source/Installer/SoupInstaller/Properties/AssemblyInfo.cs
+++ Source/Installer/SoupInstaller/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SoupInstaller")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("Copyright © 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
