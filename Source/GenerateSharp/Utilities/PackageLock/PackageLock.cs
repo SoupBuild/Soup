7188093c57b89dbf7079f785547a81bb941e26b9
Matthew Asplund <mwasplund@outlook.com>
2022-01-11 22:20:35 -0800

    create package lock file type

---
+++ Source/GenerateSharp/Utilities/PackageLock/PackageLock.cs
@@ -0,0 +1,96 @@
+﻿// <copyright file="PackageLock.cs" company="Soup">
+// Copyright (c) Soup. All rights reserved.
+// </copyright>
+
+using Soup.Build.Runtime;
+using System;
+using Tomlyn.Syntax;
+
+namespace Soup.Build.Utilities
+{
+	/// <summary>
+	/// The package lock container
+	/// </summary>
+	public class PackageLock
+	{
+		private static string Property_Projects => "Projects";
+
+		private ValueTable _table;
+
+		private DocumentSyntax? _mirrorSyntax;
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="PackageLock"/> class.
+		/// </summary>
+		public PackageLock()
+		{
+			_table = new ValueTable();
+			_mirrorSyntax = null;
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="PackageLock"/> class.
+		/// </summary>
+		public PackageLock(ValueTable table, DocumentSyntax mirrorSystax)
+		{
+			_table = table;
+			_mirrorSyntax = mirrorSystax;
+		}
+
+		/// <summary>
+		/// Gets or sets the table of projects
+		/// </summary>
+		public bool HasProjects()
+		{
+			return HasValue(_table, Property_Projects);
+		}
+
+		public IValueList GetProjects()
+		{
+			if (!HasProjects())
+				throw new InvalidOperationException("No projects.");
+
+			var values = GetValue(_table, Property_Projects).AsList();
+			return values;
+		}
+
+
+		/// <summary>
+		/// Raw access
+		/// </summary>
+		public ValueTable Table => _table;
+
+		public DocumentSyntax? MirrorSyntax => _mirrorSyntax;
+
+		private bool HasValue(IValueTable table, string key)
+		{
+			return table.ContainsKey(key);
+		}
+
+		private IValue GetValue(IValueTable table, string key)
+		{
+			if (table.TryGetValue(key, out var value))
+			{
+				return value;
+			}
+			else
+			{
+				throw new InvalidOperationException("Requested recipe value does not exist in the table.");
+			}
+		}
+
+		private IValue EnsureValue(IValueTable table, string key, IValue value)
+		{
+			if (table.ContainsKey(key))
+			{
+				table.Add(key, value);
+			}
+			else
+			{
+				table.Add(key, value);
+			}
+
+			return value;
+		}
+	}
+}
---
+++ Source/GenerateSharp/Utilities/PackageLock/PackageLockExtensions.cs
@@ -0,0 +1,70 @@
+﻿// <copyright file="PackageLockExtensions.cs" company="Soup">
+// Copyright (c) Soup. All rights reserved.
+// </copyright>
+
+using Opal;
+using Opal.System;
+using System;
+using System.Threading.Tasks;
+
+namespace Soup.Build.Utilities
+{
+	/// <summary>
+	/// The package lock extensions
+	/// </summary>
+	public static class PackageLockExtensions
+	{
+		/// <summary>
+		/// Attempt to load the package lock from file
+		/// </summary>
+		public static async Task<(bool IsSuccess, PackageLock Result)> TryLoadFromFileAsync(Path packageLockFile)
+		{
+			// Verify the requested file exists
+			Log.Diag("Load Package Lock: " + packageLockFile.ToString());
+			if (!System.IO.File.Exists(packageLockFile.ToString()))
+			{
+				Log.Info("Package Lock file does not exist.");
+				return (false, new PackageLock());
+			}
+
+			// Open the file to read from
+			using (var fileStream = System.IO.File.OpenRead(packageLockFile.ToString()))
+			using (var reader = new System.IO.StreamReader(fileStream))
+			{
+				// Read the contents of the recipe file
+				try
+				{
+					var result = ValueTableTomlUtilities.Deserialize(
+						packageLockFile,
+						await reader.ReadToEndAsync());
+					return (true, new PackageLock(result.Table, result.Root));
+				}
+				catch (Exception ex)
+				{
+					Log.Error($"Deserialize Threw: {ex.Message}");
+					Log.Info("Failed to parse Package Lock.");
+					return (false, new PackageLock());
+				}
+			}
+		}
+
+		/// <summary>
+		/// Save the package lock to file
+		/// </summary>
+		public static async Task SaveToFileAsync(
+			Path packageLockFile,
+			PackageLock packageLock)
+		{
+			// Open the file to write to
+			var file = LifetimeManager.Get<IFileSystem>().OpenWrite(packageLockFile, true);
+
+			// Serialize the contents of the recipe
+			var documentSyntax = packageLock.MirrorSyntax;
+			if (documentSyntax == null)
+				throw new ArgumentException("The provided package lock does not have a mirrored syntax tree.", nameof(packageLock));
+
+			// Write the recipe to the file stream
+			await ValueTableTomlUtilities.SerializeAsync(documentSyntax, file.GetOutStream());
+		}
+	}
+}
