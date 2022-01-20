9bc61451ba4d0b681e4d84834d34db9a35649082
Matthew Asplund <mwasplund@outlook.com>
2022-01-19 21:04:25 -0800

    Small

--- Samples/Cpp/WindowsProject/framework.h
+++ Samples/Cpp/WindowsProject/framework.h
@@ -1,11 +1,11 @@
-// header.h : include file for standard system include files,
+// framework.h : include file for standard system include files,
 // or project specific include files
 //
 
 #pragma once
 
 #include "targetver.h"
-#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+#define WIN32_LEAN_AND_MEAN // Exclude rarely-used stuff from Windows headers
 // Windows Header Files
 #include <windows.h>
 // C RunTime Header Files
