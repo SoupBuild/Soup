f596c9fbe2f7b0286cddb53a59f767c098d7e566
Matthew Asplund <mwasplund@outlook.com>
2022-03-21 12:55:12 -0700

    Bump version

--- Source/Client/CLI/Source/Commands/VersionCommand.h
+++ Source/Client/CLI/Source/Commands/VersionCommand.h
@@ -31,7 +31,7 @@ namespace Soup::Client
 
 			// TODO var version = Assembly.GetExecutingAssembly().GetName().Version;
 			// Log::Message($"{version.Major}.{version.Minor}.{version.Build}");
-			Log::HighPriority("0.17.3");
+			Log::HighPriority("0.17.4");
 		}
 
 	private:
