17c2d302a5797eb1ff81976fb7d6171bd6e0edc4
Matthew Asplund <mwasplund@outlook.com>
2022-09-05 00:59:19 -0500

    Add real tests

--- Source/Client/Core/UnitTests/gen/Build/RecipeBuildLocationManagerTests.gen.h
+++ Source/Client/Core/UnitTests/gen/Build/RecipeBuildLocationManagerTests.gen.h
@@ -7,6 +7,7 @@ TestState RunRecipeBuildLocationManagerTests()
 	auto testClass = std::make_shared<Soup::Core::UnitTests::RecipeBuildLocationManagerTests>();
 	TestState state = { 0, 0 };
 	state += Soup::Test::RunTest(className, "GetOutputDirectory_Simple", [&testClass]() { testClass->GetOutputDirectory_Simple(); });
+	state += Soup::Test::RunTest(className, "GetOutputDirectory_RootRecipe", [&testClass]() { testClass->GetOutputDirectory_RootRecipe(); });
 
 	return state;
 }
