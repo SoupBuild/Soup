10f1cdd6341ca5744b93998b90ac0473ac54e4c8
Matthew Asplund <mwasplund@outlook.com>
2022-09-07 23:08:58 -0500

    Update version and installer

--- Scripts/soup.cmd
+++ Scripts/soup.cmd
@@ -5,7 +5,7 @@ SET OutDir=%RootDir%\out
 SET RunDir=%OutDir%\run
 REM - Use a copy of the final binary in case we are re-buiding itself
 robocopy %ScriptsDir%\Install\ %RunDir%\ /MIR /NJH /NJS /NDL > NUL
-robocopy %OutDir%\C++\Soup\0.19.3\txTMowfPh1V3rPmbvNBmBW9Z8Jg\bin\ %RunDir%\Soup\ /MIR /NJH /NJS /NDL > NUL
+robocopy %OutDir%\C++\Soup\0.19.4\txTMowfPh1V3rPmbvNBmBW9Z8Jg\bin\ %RunDir%\Soup\ /MIR /NJH /NJS /NDL > NUL
 robocopy %OutDir%\msbuild\bin\Soup.Build.Generate\Release\net6.0\ %RunDir%\Soup\Generate\ /MIR /NJH /NJS /NDL > NUL
 robocopy C:\Users\mwasp\.soup\out\C#\Soup.Cpp\0.2.2\txTMowfPh1V3rPmbvNBmBW9Z8Jg\bin\ %RunDir%\Soup\Extensions\Soup.Cpp\0.2.2\ /MIR /NJH /NJS /NDL > NUL
 robocopy C:\Users\mwasp\.soup\out\C#\Soup.CSharp\0.5.2\txTMowfPh1V3rPmbvNBmBW9Z8Jg\bin %RunDir%\Soup\Extensions\Soup.CSharp\0.5.2\ /MIR /NJH /NJS /NDL > NUL
--- Scripts/soupd.cmd
+++ Scripts/soupd.cmd
@@ -5,7 +5,7 @@ SET OutDir=%RootDir%\out
 SET RunDir=%OutDir%\run
 REM - Use a copy of the final binary in case we are re-buiding itself
 robocopy %ScriptsDir%\Install\ %RunDir%\ /MIR /NJH /NJS /NDL > NUL
-robocopy %OutDir%\C++\Soup\0.19.3\J3mu4cpISw6nDaCPED8gkqZ-q84\bin\ %RunDir%\Soup\ /MIR /NJH /NJS /NDL > NUL
+robocopy %OutDir%\C++\Soup\0.19.4\J3mu4cpISw6nDaCPED8gkqZ-q84\bin\ %RunDir%\Soup\ /MIR /NJH /NJS /NDL > NUL
 robocopy %OutDir%\msbuild\bin\Soup.Build.Generate\Debug\net6.0\ %RunDir%\Soup\Generate\ /MIR /NJH /NJS /NDL > NUL
 robocopy C:\Users\mwasp\.soup\out\C#\Soup.Cpp\0.2.2\J3mu4cpISw6nDaCPED8gkqZ-q84\bin %RunDir%\Soup\Extensions\Soup.Cpp\0.2.2\ /MIR /NJH /NJS /NDL > NUL
 robocopy C:\Users\mwasp\.soup\out\C#\Soup.CSharp\0.5.2\J3mu4cpISw6nDaCPED8gkqZ-q84\bin %RunDir%\Soup\Extensions\Soup.CSharp\0.5.2\ /MIR /NJH /NJS /NDL > NUL
