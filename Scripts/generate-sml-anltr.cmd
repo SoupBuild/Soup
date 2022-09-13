a98b9a21d87749c4b569e60d0fd0913260170886
Matthew Asplund <mwasplund@outlook.com>
2022-09-12 20:20:05 -0500

    Add Antlr Generator for SML C#

---
+++ Scripts/generate-sml-anltr.cmd
@@ -0,0 +1,13 @@
+@echo off
+SET Flavor=%1
+SET ScriptsDir=%~dp0
+SET RootDir=%ScriptsDir%..\
+SET GrammarSourceDir=%RootDir%\Source\Grammar
+SET CSharpSourceDir=%RootDir%\Source\GenerateSharp\Utilities\SML
+SET JAVA="C:\Program Files\Microsoft\jdk-11.0.12.7-hotspot\bin\java.exe"
+SET ANLTR=%JAVA% -jar C:\Users\mwasp\Desktop\antlr-4.11.1-complete.jar
+
+pushd %GrammarSourceDir%
+echo %ANLTR% -Dlanguage=CSharp SML.g4 -o %CSharpSourceDir%
+call %ANLTR% -Dlanguage=CSharp SML.g4 -o %CSharpSourceDir%
+popd
---
+++ Source/Grammar/SML.g4
@@ -0,0 +1,9 @@
+grammar SML;
+prog:	expr EOF ;
+expr:	expr ('*'|'/') expr
+    |	expr ('+'|'-') expr
+    |	INT
+    |	'(' expr ')'
+    ;
+NEWLINE : [\r\n]+ -> skip;
+INT     : [0-9]+ ;
