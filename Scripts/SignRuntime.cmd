6c067192246380a24f280f6aeb6beb2d37cb3ec3
Matthew Asplund <mwasplund@gmail.com>
2022-09-16 11:30:13 -0500

    Random cleanup (#134)
    * Random cleanup
    
    * Fix swhere properties
    
    * Bump version
    
    Co-authored-by: Matthew Asplund <mwasplund@outlook.com>

--- Scripts/SignRuntime.cmd
+++ Scripts/SignRuntime.cmd
@@ -6,13 +6,11 @@ SET RunDir=%OutDir%\run
 
 SET CertPath=%1
 
+signtool sign /tr http://timestamp.sectigo.com /td sha256 /fd sha256 /f %CertPath%  %RunDir%\*.exe
 signtool sign /tr http://timestamp.sectigo.com /td sha256 /fd sha256 /f %CertPath%  %RunDir%\Soup\*.dll
 signtool sign /tr http://timestamp.sectigo.com /td sha256 /fd sha256 /f %CertPath%  %RunDir%\Soup\*.exe
+signtool sign /tr http://timestamp.sectigo.com /td sha256 /fd sha256 /f %CertPath%  %RunDir%\Soup\Extensions\Soup.Cpp\0.3.0\*.dll
+signtool sign /tr http://timestamp.sectigo.com /td sha256 /fd sha256 /f %CertPath%  %RunDir%\Soup\Extensions\Soup.CSharp\0.6.0\*.dll
 signtool sign /tr http://timestamp.sectigo.com /td sha256 /fd sha256 /f %CertPath%  %RunDir%\Soup\Generate\*.dll
 signtool sign /tr http://timestamp.sectigo.com /td sha256 /fd sha256 /f %CertPath%  %RunDir%\Soup\Generate\*.exe
-signtool sign /tr http://timestamp.sectigo.com /td sha256 /fd sha256 /f %CertPath%  %RunDir%\Soup\Generate\Extensions\Soup.Cpp\0.2.2\*.dll
-signtool sign /tr http://timestamp.sectigo.com /td sha256 /fd sha256 /f %CertPath%  %RunDir%\Soup\Generate\Extensions\Soup.CSharp\0.5.1\*.dll
-signtool sign /tr http://timestamp.sectigo.com /td sha256 /fd sha256 /f %CertPath%  %RunDir%\Soup\PackageManager\*.dll
 signtool sign /tr http://timestamp.sectigo.com /td sha256 /fd sha256 /f %CertPath%  %RunDir%\Soup\PackageManager\*.exe
-signtool sign /tr http://timestamp.sectigo.com /td sha256 /fd sha256 /f %CertPath%  %RunDir%\Swhere\*.dll
-signtool sign /tr http://timestamp.sectigo.com /td sha256 /fd sha256 /f %CertPath%  %RunDir%\Swhere\*.exe
--- Source/Installer/SoupInstaller/Setup.cs
+++ Source/Installer/SoupInstaller/Setup.cs
@@ -55,7 +55,7 @@ class Script
 		};
 
 		// Upgrade values
-		project.Version = new Version(0, 20, 0);
+		project.Version = new Version(0, 20, 1);
 
 		Compiler.BuildMsi(project);
 	}
