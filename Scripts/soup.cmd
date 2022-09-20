6cc1341556c662a89fb85cc506ccd56f819a014c
Matthew Asplund <mwasplund@outlook.com>
2022-09-19 23:58:49 -0500

    Convert back to using non-single file to save on size

--- Scripts/soup.cmd
+++ Scripts/soup.cmd
@@ -5,10 +5,10 @@ SET OutDir=%RootDir%\out
 SET RunDir=%OutDir%\run
 REM - Use a copy of the final binary in case we are re-buiding itself
 robocopy %ScriptsDir%\Install\ %RunDir%\ /MIR /NJH /NJS /NDL > NUL
-robocopy %OutDir%\C++\Soup\0.21.0\txTMowfPh1V3rPmbvNBmBW9Z8Jg\bin\ %RunDir%\Soup\ /MIR /NJH /NJS /NDL > NUL
+robocopy %OutDir%\C++\Soup\0.21.1\txTMowfPh1V3rPmbvNBmBW9Z8Jg\bin\ %RunDir%\Soup\ /MIR /NJH /NJS /NDL > NUL
 robocopy %OutDir%\msbuild\bin\Soup.Build.Generate\Release\net6.0\win-x64\publish\ %RunDir%\Soup\Generate\ /MIR /NJH /NJS /NDL > NUL
 robocopy C:\Users\mwasp\.soup\out\C#\Soup.Cpp\0.3.0\txTMowfPh1V3rPmbvNBmBW9Z8Jg\bin\ %RunDir%\Soup\Extensions\Soup.Cpp\0.3.0\ /MIR /NJH /NJS /NDL > NUL
 robocopy C:\Users\mwasp\.soup\out\C#\Soup.CSharp\0.6.0\txTMowfPh1V3rPmbvNBmBW9Z8Jg\bin\ %RunDir%\Soup\Extensions\Soup.CSharp\0.6.0\ /MIR /NJH /NJS /NDL > NUL
-robocopy %OutDir%\msbuild\bin\Soup.Build.PackageManager\Release\net6.0\win-x64\publish\ %RunDir%\Soup\PackageManager\ Soup.Build.PackageManager.exe /NJH /NJS /NDL > NUL
+robocopy %OutDir%\msbuild\bin\Soup.Build.PackageManager\Release\net6.0\win-x64\publish\ %RunDir%\Soup\PackageManager\ /MIR /NJH /NJS /NDL > NUL
 robocopy %OutDir%\msbuild\bin\Swhere\Release\net6.0\win-x64\publish\ %RunDir%\ swhere.exe /NJH /NJS /NDL > NUL
 %RunDir%\Soup.cmd %*
