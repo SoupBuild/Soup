c9295ab2d42ef1bfcf63deec4b52a062b036ec0f
Matthew Asplund <mwasplund@outlook.com>
2022-10-11 10:48:22 -0500

    Fix incorrect version references

--- Source/Client/Core/Source/Build/PackageProvider.h
+++ Source/Client/Core/Source/Build/PackageProvider.h
@@ -43,11 +43,11 @@ namespace Soup::Core
 		const std::string _buildDependencyType = "Build";
 		const Path _builtInExtensionPath = Path("Extensions/");
 		const std::string _builtInCppLanguage = "C++";
-		const std::string _builtInCppExtensionVersion = "0.3.0";
+		const std::string _builtInCppExtensionVersion = "0.4.0";
 		const Path _builtInCppExtensionPath = Path("Soup.Cpp/");
 		const Path _builtInCppExtensionFilename = Path("Soup.Cpp.dll");
 		const std::string _builtInCSharpLanguage = "C#";
-		const std::string _builtInCSharpExtensionVersion = "0.6.0";
+		const std::string _builtInCSharpExtensionVersion = "0.7.0";
 		const Path _builtInCSharpExtensionPath = Path("Soup.CSharp/");
 		const Path _builtInCSharpExtensionFilename = Path("Soup.CSharp.dll");
 
