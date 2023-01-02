7680b635785e49c16d704068f7e6f0017d86b815
Matthew Asplund <mwasplund@outlook.com>
2023-01-01 19:50:24 -0600

    Hook in Soup module and callbacks

--- Source/Generate/Main.cpp
+++ Source/Generate/Main.cpp
@@ -1,17 +1,95 @@
-//For more details, visit https://wren.io/embedding/
+// <copyright file="Main.cpp" company="Soup">
+// Copyright (c) Soup. All rights reserved.
+// </copyright>
 
 #include <stdio.h>
+#include <stdexcept>
 #include <fstream>
 #include <iostream>
+
 #include "wren.hpp"
 
-static void writeFn(WrenVM* vm, const char* text)
+static const char* soupModuleSource =
+	"class Soup {\n"
+	"	static debug(message) {\n"
+	"		if (!(message is String)) Fiber.abort(\"Message must be a string.\")\n"
+	"		logMessage_(message, 1)\n"
+	"	}\n"
+	"\n"
+	"	static warning(message) {\n"
+	"		if (!(message is String)) Fiber.abort(\"Message must be a string.\")\n"
+	"		logMessage_(message, 2)\n"
+	"	}\n"
+	"\n"
+	"	static error(message) {\n"
+	"		if (!(message is String)) Fiber.abort(\"Message must be a string.\")\n"
+	"		logMessage_(message, 3)\n"
+	"	}\n"
+	"\n"
+	"	foreign static logMessage_(message, level)\n"
+	"}\n";
+
+
+void SoupLogMessage(WrenVM* vm)
+{
+	auto message = wrenGetSlotString(vm, 1);
+	auto level = wrenGetSlotDouble(vm, 2);
+	std::cout << "LogMessage: [" << level << "] " << message << std::endl;
+}
+
+const char* WrenSoupSource()
+{
+	return soupModuleSource;
+}
+
+WrenForeignMethodFn WrenBindForeignMethod(
+	WrenVM* vm,
+	const char* module,
+	const char* className,
+	bool isStatic,
+	const char* signature)
+{
+	(vm);
+	(module);
+	auto classNameValue = std::string_view(className);
+	auto signatureValue = std::string_view(signature);
+
+	// There is only one foreign method in the soup module.
+	if (classNameValue == "Soup" && isStatic)
+	{
+		if (signatureValue == "logMessage_(_,_)")
+		{
+			return SoupLogMessage;
+		}
+	}
+
+	return nullptr;
+}
+
+static WrenLoadModuleResult LoadModule(WrenVM* vm, const char* module)
+{
+	(vm);
+	auto moduleName = std::string_view(module);
+
+	WrenLoadModuleResult result = {0};
+	result.onComplete = nullptr;
+
+	// Inject Soup module
+	if (moduleName == "soup")
+	{
+		result.source = WrenSoupSource();
+	}
+
+	return result;
+}
+
+static void WriteCallback(WrenVM* vm, const char* text)
 {
 	(vm);
 	printf("%s", text);
 }
 
-void errorFn(
+void ErrorCallback(
 	WrenVM* vm,
 	WrenErrorType errorType,
 	const char* module,
@@ -39,41 +117,81 @@ void errorFn(
 	}
 }
 
-int main()
+void ThrowIfFailed(WrenInterpretResult result)
 {
+	switch (result)
+	{
+		case WREN_RESULT_COMPILE_ERROR:
+		{
+			throw std::runtime_error("Compile Error!");
+		}
+		case WREN_RESULT_RUNTIME_ERROR:
+		{
+			throw std::runtime_error( "Runtime Error!");
+		}
+		case WREN_RESULT_SUCCESS:
+		{
+			std::cout << "Success!" << std::endl;
+			break;
+		}
+	}
+}
+
+void Run()
+{
+	// Configure the Wren Virtual Machine
 	WrenConfiguration config;
 	wrenInitConfiguration(&config);
-	config.writeFn = &writeFn;
-	config.errorFn = &errorFn;
-	WrenVM* vm = wrenNewVM(&config);
+	config.loadModuleFn = &LoadModule;
+	config.bindForeignMethodFn = &WrenBindForeignMethod;
+	config.writeFn = &WriteCallback;
+	config.errorFn = &ErrorCallback;
 
-	const char* module = "main";
-	
+	// Create the VM
+	auto vm = wrenNewVM(&config);
+
+	// Load the script
 	std::ifstream scriptFile("Test.wren");
 	auto script = std::string(
 		std::istreambuf_iterator<char>(scriptFile),
 		std::istreambuf_iterator<char>());
 
-	WrenInterpretResult result = wrenInterpret(vm, module, script.c_str());
+	// Interpret the script
+	const char* module = "main";
+	auto result = wrenInterpret(vm, module, script.c_str());
+	ThrowIfFailed(result);
 
-	switch (result)
-	{
-		case WREN_RESULT_COMPILE_ERROR:
-		{
-			printf("Compile Error!\n");
-			break;
-		}
-		case WREN_RESULT_RUNTIME_ERROR:
-		{
-			printf("Runtime Error!\n");
-			break;
-		}
-		case WREN_RESULT_SUCCESS:
-		{
-			printf("Success!\n");
-			break;
-		}
+	wrenEnsureSlots(vm, 1);
+	wrenGetVariable(vm, module, "TestClass", 0);
+
+	auto type = wrenGetSlotType(vm, 0);
+	if (type != WREN_TYPE_UNKNOWN) {
+		throw std::runtime_error("Missing Class TestClass");
 	}
 
+	auto testClassHandle = wrenGetSlotHandle(vm, 0);
+
+	auto doStuffMethodHandle = wrenMakeCallHandle(vm, "DoStuff()");
+
+	wrenSetSlotHandle(vm, 0, testClassHandle);
+	result = wrenCall(vm, doStuffMethodHandle);
+	ThrowIfFailed(result);
+
+	wrenReleaseHandle(vm, doStuffMethodHandle);
+	wrenReleaseHandle(vm, testClassHandle);
+
 	wrenFreeVM(vm);
+}
+
+int main()
+{
+	try
+	{
+		Run();
+	}
+	catch (const std::exception& ex)
+	{
+		std::cout << ex.what() << std::endl;
+		return -1;
+	}
 }
--- Source/Generate/Test.wren
+++ Source/Generate/Test.wren
@@ -1,1 +1,14 @@
 System.print("I am running in a VM!")
+
+import "soup" for Soup
+
+class TestClass {
+	static RunBefore { [] }
+
+	static DoStuff() {
+		Soup.debug("I am running in a method!")
+		Soup.warning("I am running in a method!")
+		Soup.error("I am running in a method!")
+		return 1
+	}
+}
