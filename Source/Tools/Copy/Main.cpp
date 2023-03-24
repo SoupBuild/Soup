db7a2b544642c84752ef43d68397c82cc77e9ad2
Matthew Asplund <mwasplund@outlook.com>
2023-03-23 20:32:14 -0500

    Build Wren

--- Source/Tools/Copy/Main.cpp
+++ Source/Tools/Copy/Main.cpp
@@ -18,7 +18,15 @@ void CopyFile(std::string_view sourcePath, std::string_view destinationPath)
 {
 	if (!CopyFileA(sourcePath.data(), destinationPath.data(), false))
 	{
-		throw std::runtime_error("Copy failed");
+		auto errorCode = GetLastError();
+		if (errorCode == ERROR_FILE_NOT_FOUND)
+		{
+			throw std::runtime_error("File does not exist");
+		}
+		else
+		{
+			throw std::runtime_error("Copy failed: " + std::to_string(errorCode));
+		}
 	}
 }
 #elif defined(__linux__)
@@ -41,18 +49,7 @@ int main(int argc, char** argv)
 	{
 		auto sourcePath = std::string_view(argv[1]);
 		auto destinationPath = std::string_view(argv[2]);
-		if (!CopyFileA(sourcePath.data(), destinationPath.data(), false))
-		{
-			auto errorCode = GetLastError();
-			if (errorCode == ERROR_FILE_NOT_FOUND)
-			{
-				throw std::runtime_error("File does not exist");
-			}
-			else
-			{
-				throw std::runtime_error("Copy failed: " + std::to_string(errorCode));
-			}
-		}
+		CopyFile(sourcePath, destinationPath);
 	}
 	catch(const std::exception& e)
 	{
