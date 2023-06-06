81ad527692d2420442acf47c6558a8e08e007a9a
Matthew Asplund <mwasplund@outlook.com>
2023-06-05 22:01:39 -0500

    Update to pull real builds from scripts

--- Scripts/Windows/soup.cmd
+++ Scripts/Windows/soup.cmd
@@ -4,6 +4,9 @@ SET ScriptsDir=%~dp0
 SET RootDir=%ScriptsDir%..\..
 SET OutDir=%RootDir%\out
 SET RunDir=%OutDir%\run
+SET SourceDir=%RootDir%\Source
+SET GlobalPackagesDir=C:\Users\mwasp\.soup\packages
+SET GlobalOutDir=C:\Users\mwasp\.soup\out
 
 SET SOUP_VERSION=0.34.0
 SET COPY_VERSION=1.0.0
@@ -16,20 +19,20 @@ REM - Use a copy of the final binary in case we are re-buiding itself
 robocopy %ScriptsDir%\Install\ %RunDir%\ /MIR /NJH /NJS /NDL > NUL
 robocopy %OutDir%\Cpp\Soup\%SOUP_VERSION%\Oltq7cGwk0Rbgy1I-3mCMDDE5yM\bin\ %RunDir%\Soup\ /MIR /NJH /NJS /NDL > NUL
 
-robocopy C:\Users\mwasp\Dev\Repos\Soup\Source\Tools\Copy\ %RunDir%\Soup\BuiltIn\copy\%COPY_VERSION%\ Recipe.sml /NJH /NJS /NDL > NUL
-robocopy C:\Users\mwasp\Dev\Repos\Soup\out\Cpp\copy\%COPY_VERSION%\Oltq7cGwk0Rbgy1I-3mCMDDE5yM\ %RunDir%\Soup\BuiltIn\copy\%COPY_VERSION%\out\ /MIR /NJH /NJS /NDL > NUL
+robocopy %SourceDir%\Tools\Copy\ %RunDir%\Soup\BuiltIn\copy\%COPY_VERSION%\ Recipe.sml /NJH /NJS /NDL > NUL
+robocopy %OutDir%\Cpp\copy\%COPY_VERSION%\Oltq7cGwk0Rbgy1I-3mCMDDE5yM\ %RunDir%\Soup\BuiltIn\copy\%COPY_VERSION%\out\ /MIR /NJH /NJS /NDL > NUL
 
-robocopy C:\Users\mwasp\Dev\Repos\Soup\Source\Tools\Mkdir\ %RunDir%\Soup\BuiltIn\mkdir\%MKDIR_VERSION%\ Recipe.sml /NJH /NJS /NDL > NUL
-robocopy C:\Users\mwasp\Dev\Repos\Soup\out\Cpp\mkdir\%MKDIR_VERSION%\Oltq7cGwk0Rbgy1I-3mCMDDE5yM\ %RunDir%\Soup\BuiltIn\mkdir\%MKDIR_VERSION%\out\ /MIR /NJH /NJS /NDL > NUL
+robocopy %SourceDir%\Tools\Mkdir\ %RunDir%\Soup\BuiltIn\mkdir\%MKDIR_VERSION%\ Recipe.sml /NJH /NJS /NDL > NUL
+robocopy %OutDir%\Cpp\mkdir\%MKDIR_VERSION%\Oltq7cGwk0Rbgy1I-3mCMDDE5yM\ %RunDir%\Soup\BuiltIn\mkdir\%MKDIR_VERSION%\out\ /MIR /NJH /NJS /NDL > NUL
 
-robocopy C:\Users\mwasp\.soup\packages\Wren\Soup.Cpp\%SOUP_CPP_VERSION%\ %RunDir%\Soup\BuiltIn\Soup.Cpp\%SOUP_CPP_VERSION%\ Recipe.sml /NJH /NJS /NDL > NUL
-robocopy C:\Users\mwasp\.soup\out\Wren\Soup.Cpp\%SOUP_CPP_VERSION%\Oltq7cGwk0Rbgy1I-3mCMDDE5yM\ %RunDir%\Soup\BuiltIn\Soup.Cpp\%SOUP_CPP_VERSION%\out\ /MIR /NJH /NJS /NDL > NUL
+robocopy %GlobalPackagesDir%\Wren\Soup.Cpp\%SOUP_CPP_VERSION%\ %RunDir%\Soup\BuiltIn\Soup.Cpp\%SOUP_CPP_VERSION%\ Recipe.sml /NJH /NJS /NDL > NUL
+robocopy %GlobalOutDir%\Wren\Soup.Cpp\%SOUP_CPP_VERSION%\Oltq7cGwk0Rbgy1I-3mCMDDE5yM\ %RunDir%\Soup\BuiltIn\Soup.Cpp\%SOUP_CPP_VERSION%\out\ /MIR /NJH /NJS /NDL > NUL
 
-robocopy C:\Users\mwasp\.soup\packages\Wren\Soup.CSharp\%SOUP_CSHARP_VERSION%\ %RunDir%\Soup\BuiltIn\Soup.CSharp\%SOUP_CSHARP_VERSION%\ Recipe.sml /NJH /NJS /NDL > NUL
-robocopy C:\Users\mwasp\.soup\out\Wren\Soup.CSharp\%SOUP_CSHARP_VERSION%\Oltq7cGwk0Rbgy1I-3mCMDDE5yM\ %RunDir%\Soup\BuiltIn\Soup.CSharp\%SOUP_CSHARP_VERSION%\out\ /MIR /NJH /NJS /NDL > NUL
+robocopy %GlobalPackagesDir%\Wren\Soup.CSharp\%SOUP_CSHARP_VERSION%\ %RunDir%\Soup\BuiltIn\Soup.CSharp\%SOUP_CSHARP_VERSION%\ Recipe.sml /NJH /NJS /NDL > NUL
+robocopy %GlobalOutDir%\Wren\Soup.CSharp\%SOUP_CSHARP_VERSION%\Oltq7cGwk0Rbgy1I-3mCMDDE5yM\ %RunDir%\Soup\BuiltIn\Soup.CSharp\%SOUP_CSHARP_VERSION%\out\ /MIR /NJH /NJS /NDL > NUL
 
-robocopy C:\Users\mwasp\.soup\packages\Wren\Soup.Wren\%SOUP_WREN_VERSION%\ %RunDir%\Soup\BuiltIn\Soup.Wren\%SOUP_WREN_VERSION%\ Recipe.sml /NJH /NJS /NDL > NUL
-robocopy C:\Users\mwasp\.soup\out\Wren\Soup.Wren\%SOUP_WREN_VERSION%\Oltq7cGwk0Rbgy1I-3mCMDDE5yM\ %RunDir%\Soup\BuiltIn\Soup.Wren\%SOUP_WREN_VERSION%\out\ /MIR /NJH /NJS /NDL > NUL
+robocopy %GlobalPackagesDir%\Wren\Soup.Wren\%SOUP_WREN_VERSION%\ %RunDir%\Soup\BuiltIn\Soup.Wren\%SOUP_WREN_VERSION%\ Recipe.sml /NJH /NJS /NDL > NUL
+robocopy %GlobalOutDir%\Wren\Soup.Wren\%SOUP_WREN_VERSION%\Oltq7cGwk0Rbgy1I-3mCMDDE5yM\ %RunDir%\Soup\BuiltIn\Soup.Wren\%SOUP_WREN_VERSION%\out\ /MIR /NJH /NJS /NDL > NUL
 
 robocopy %OutDir%\msbuild\bin\Soup.Build.PackageManager\Release\net6.0-windows10.0.17763.0\win-x64\publish\ %RunDir%\Soup\PackageManager\ /MIR /NJH /NJS /NDL > NUL
 
