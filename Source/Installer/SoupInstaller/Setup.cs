53a8868501203f6fd49dae12264df357a9c86fb7
Matthew Asplund <mwasplund@gmail.com>
2023-07-07 11:05:16 -0500

    Enable c samples (#209)
    * Enable c samples
    
    * Fix unit tests

--- Source/Installer/SoupInstaller/Setup.cs
+++ Source/Installer/SoupInstaller/Setup.cs
@@ -5,7 +5,7 @@ class Script
 {
 	static public void Main()
 	{
-		var soupVersion = new Version(0, 34, 0);
+		var soupVersion = new Version(0, 35, 0);
 
 		var soupOutFolder = "../../../out";
 		var soupRunFolder = $"{soupOutFolder}/run";
