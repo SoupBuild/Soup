604a3bb15fe4d150abb351017c214d77c828e5cd
Matthew Asplund <mwasplund@outlook.com>
2023-07-06 20:55:44 -0500

    Bump version

--- Scripts/Windows/release.cmd
+++ Scripts/Windows/release.cmd
@@ -5,6 +5,6 @@ SET RootDir=%ScriptsDir%..\..
 SET OutDir=%RootDir%\out
 SET RunDir=%OutDir%\run
 
-SET SOUP_VERSION=0.34.0
+SET SOUP_VERSION=0.35.0
 
 tar -a -cf %OutDir%\soup-build-%SOUP_VERSION%-windows-x64.zip -C %RunDir% *
