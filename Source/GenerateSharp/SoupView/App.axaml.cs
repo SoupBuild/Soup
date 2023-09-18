f9a46be01184699423124b64616416efc17812bd
Matthew Asplund <mwasplund@outlook.com>
2023-09-17 20:33:40 -0500

    Cleanup sample

--- Source/GenerateSharp/SoupView/App.axaml.cs
+++ Source/GenerateSharp/SoupView/App.axaml.cs
@@ -2,22 +2,22 @@ using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
 
-namespace SoupView;
+namespace Soup.View;
 
 public partial class App : Application
 {
-    public override void Initialize()
-    {
-        AvaloniaXamlLoader.Load(this);
-    }
+	public override void Initialize()
+	{
+		AvaloniaXamlLoader.Load(this);
+	}
 
-    public override void OnFrameworkInitializationCompleted()
-    {
-        if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
-        {
-            desktop.MainWindow = new MainWindow();
-        }
+	public override void OnFrameworkInitializationCompleted()
+	{
+		if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+		{
+			desktop.MainWindow = new MainWindow();
+		}
 
-        base.OnFrameworkInitializationCompleted();
-    }
+		base.OnFrameworkInitializationCompleted();
+	}
 }
--- Source/GenerateSharp/SoupView/MainWindow.axaml.cs
+++ Source/GenerateSharp/SoupView/MainWindow.axaml.cs
@@ -1,11 +1,11 @@
 using Avalonia.Controls;
 
-namespace SoupView;
+namespace Soup.View;
 
 public partial class MainWindow : Window
 {
-    public MainWindow()
-    {
-        InitializeComponent();
-    }
+	public MainWindow()
+	{
+		InitializeComponent();
+	}
 }
