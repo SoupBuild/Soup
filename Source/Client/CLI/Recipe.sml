6af6844739c57a2f49d783937af32e7cc2266883
Matthew Asplund <mwasplund@outlook.com>
2024-02-17 10:05:41 -0600

    Bump version

--- Source/Client/CLI/Recipe.sml
+++ Source/Client/CLI/Recipe.sml
@@ -1,5 +1,5 @@
 Name: 'Soup'
-Version: '0.37.0'
+Version: '0.37.1'
 Language: 'C++|0'
 
 Type: 'Executable'
--- Source/Installer/SoupInstaller/Setup.cs
+++ Source/Installer/SoupInstaller/Setup.cs
@@ -5,7 +5,7 @@ class Script
 {
 	static public void Main()
 	{
-		var soupVersion = new Version(0, 37, 0);
+		var soupVersion = new Version(0, 37, 1);
 
 		var soupOutFolder = "../../../out";
 		var soupRunFolder = $"{soupOutFolder}/run";
