5ee8d8518b73cd06f7f6e014ec3a46acdf5d5df1
Matthew Asplund <mwasplund@outlook.com>
2024-03-16 20:49:26 -0500

    Create basics

---
+++ Source/GenerateSharp/SoupBootstrap/Program.cs
@@ -0,0 +1,55 @@
+﻿// <copyright file="Program.cs" company="Soup">
+// Copyright (c) Soup. All rights reserved.
+// </copyright>
+
+using Opal;
+using Opal.System;
+using Path = Opal.Path;
+
+namespace Soup.Build.Discover;
+
+public static class Program
+{
+	public static async Task<int> Main(string[] args)
+	{
+		try
+		{
+			var traceFlags =
+				TraceEvents.Information |
+				TraceEvents.HighPriority |
+				TraceEvents.Critical |
+				TraceEvents.Warning |
+				TraceEvents.Error;
+			Log.RegisterListener(new ConsoleTraceListener(new EventTypeFilter(traceFlags), false, false));
+			LifetimeManager.RegisterSingleton<IFileSystem, RuntimeFileSystem>();
+
+			LifetimeManager.RegisterSingleton<IProcessManager, RuntimeProcessManager>();
+
+			Path rootDirectory;
+			if (args.Length == 1)
+			{
+				rootDirectory = new Path(args[0]);
+			}
+			else
+			{
+				PrintUsage();
+				return -1;
+			}
+
+			var script = rootDirectory + new Path("build");
+			var builder = new ScriptBuilder(script);
+			await builder.BuildScriptAsync();
+
+			return 0;
+		}
+		catch (HandledException)
+		{
+			return -1;
+		}
+	}
+
+	private static void PrintUsage()
+	{
+		Log.Info("soupbootstrap [path]");
+	}
+}
---
+++ Source/GenerateSharp/SoupBootstrap/ScriptBuilder.cs
@@ -0,0 +1,33 @@
+﻿// <copyright file="Program.cs" company="Soup">
+// Copyright (c) Soup. All rights reserved.
+// </copyright>
+
+using Opal;
+using Opal.System;
+using Path = Opal.Path;
+
+namespace Soup.Build.Discover;
+
+public class ScriptBuilder
+{
+	private readonly Path scriptPath;
+
+	public ScriptBuilder(Path scriptPath)
+	{
+		this.scriptPath = scriptPath;
+	}
+
+	public async Task BuildScriptAsync()
+	{
+		using var file = LifetimeManager.Get<IFileSystem>().OpenWrite(this.scriptPath, true);
+
+		using var writer = new StreamWriter(file.GetOutStream());
+
+		await writer.WriteLineAsync("#!/bin/bash");
+		await writer.WriteLineAsync();
+		await writer.WriteLineAsync();
+		await writer.WriteLineAsync("# Stop on first error");
+		await writer.WriteLineAsync("set - e");
+		await writer.WriteLineAsync();
+	}
+}
