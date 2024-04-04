e52788180c7b29ffc00bd2deec06dbdef6d380ba
Matthew Asplund <mwasplund@outlook.com>
2024-04-04 12:11:51 -0500

    Merge swhere

---
+++ Source/GenerateSharp/Migrate/Program.cs
@@ -0,0 +1,51 @@
+﻿// <copyright file="Program.cs" company="Soup">
+// Copyright (c) Soup. All rights reserved.
+// </copyright>
+
+using Opal;
+using Opal.System;
+
+namespace Soup.Build.Discover;
+
+public static class Program
+{
+	public static async Task<int> Main(string[] args)
+	{
+		try
+		{
+			var traceFlags =
+				TraceEvents.Information |
+				TraceEvents.HighPriority |
+				TraceEvents.Critical |
+				TraceEvents.Warning |
+				TraceEvents.Error;
+			Log.RegisterListener(new ConsoleTraceListener(new EventTypeFilter(traceFlags), false, false));
+			LifetimeManager.RegisterSingleton<IFileSystem, RuntimeFileSystem>();
+
+			LifetimeManager.RegisterSingleton<IProcessManager, RuntimeProcessManager>();
+
+			if (args.Length == 0)
+			{
+			}
+			else
+			{
+				PrintUsage();
+				return -1;
+			}
+
+			await Task.CompletedTask;
+			// await SwhereManager.DiscoverAsync(includePrerelease);
+
+			return 0;
+		}
+		catch (HandledException)
+		{
+			return -1;
+		}
+	}
+
+	private static void PrintUsage()
+	{
+		Log.Info("swhere [-prerelease]");
+	}
+}
---
+++ Source/GenerateSharp/Migrate/Recipe.sml
@@ -0,0 +1,15 @@
+Name: 'Migrate'
+Language: 'C#|0.1'
+Version: '1.0.0'
+Type: 'Executable'
+Source: [
+	'Program.cs'
+]
+
+Dependencies: {
+	Runtime: [
+		'../Swhere.Core/'
+		'../Utilities/'
+		'../Opal/'
+	]
+}
