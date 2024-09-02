0a2cb2480367587f177e6028a84e6b24afa9ec3e
Matthew Asplund <mwasplund@outlook.com>
2024-09-02 16:20:44 -0500

    Add up to date test

--- Source/Client/Core/UnitTests/gen/Build/BuildEngineTests.gen.h
+++ Source/Client/Core/UnitTests/gen/Build/BuildEngineTests.gen.h
@@ -7,6 +7,7 @@ TestState RunBuildEngineTests()
 	auto testClass = std::make_shared<Soup::Core::UnitTests::BuildEngineTests>();
 	TestState state = { 0, 0 };
 	state += Soup::Test::RunTest(className, "Execute_NoDependencies", [&testClass]() { testClass->Execute_NoDependencies(); });
+	state += Soup::Test::RunTest(className, "Execute_NoDependencies_UpToDate", [&testClass]() { testClass->Execute_NoDependencies_UpToDate(); });
 
 	return state;
 }
