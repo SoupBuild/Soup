1c4a9a02db76d7783bb25bcdf15cb86cc8b0808d
Matthew Asplund <mwasplund@outlook.com>
2024-09-21 03:37:55 -0500

    Update version

--- Scripts/Windows/SignInstaller.cmd
+++ Scripts/Windows/SignInstaller.cmd
@@ -6,6 +6,6 @@ SET OutDir=%RootDir%\out
 
 SET SIGN_COMMAND=signtool sign /n "Open Source Developer, Matthew Asplund" /t http://time.certum.pl /fd sha1 /v
 
-SET SignFiles=%OutDir%\soup-build-0.39.0-windows-x64.msi
+SET SignFiles=%OutDir%\soup-build-0.39.1-windows-x64.msi
 
 %SIGN_COMMAND% %SignFiles%
--- Source/Client/CLI/Recipe.sml
+++ Source/Client/CLI/Recipe.sml
@@ -1,5 +1,5 @@
 Name: 'Soup'
-Version: 0.39.0
+Version: 0.39.1
 Language: (C++@0)
 Type: 'Executable'
 Source: [
