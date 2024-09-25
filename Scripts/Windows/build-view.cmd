ea7f9d2c3dae4e73c55b5296b2c671577168680b
Matthew Asplund <mwasplund@outlook.com>
2024-09-25 08:11:00 -0500

    Fix msbuild paths

--- Scripts/Windows/build-view.cmd
+++ Scripts/Windows/build-view.cmd
@@ -5,12 +5,11 @@ SET ScriptsDir=%~dp0
 SET RootDir=%ScriptsDir%..\..
 SET SourceDir=%RootDir%\Source
 SET SWhereDir=%SourceDir%\GenerateSharp\SoupView
-SET OutDir=%RootDir%\out
 
 REM - Cleanup old publish to work around bug in publish
-rmdir /S /Q %OutDir%\msbuild\bin\SoupView\%Flavor%\net8.0\win-x64\publish\
+rmdir /S /Q %RootDir%\out\bin\SoupView\%Flavor%\net8.0\win-x64\publish\
 
-REM - Build SWhere tool
-echo dotnet publish %SWhereDir% -c %Flavor% -f net8.0 -r win-x64 --self-contained
-call dotnet publish %SWhereDir% -c %Flavor% -f net8.0 -r win-x64 --self-contained
+REM - Publish SWhere tool
+echo dotnet publish %SWhereDir% -c %Flavor% -f net8.0 -r win-x64 --self-contained true
+dotnet publish %SWhereDir% -c %Flavor% -f net8.0 -r win-x64 --self-contained true
 if %ERRORLEVEL% NEQ  0 exit /B %ERRORLEVEL%
