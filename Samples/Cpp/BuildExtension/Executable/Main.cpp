c8622bf9b85bd94daa71824bbd21bec12f357b99
mwasplund <mwasplund@outlook.com>
2024-10-21 20:39:04 -0500

    Add tool

--- Samples/Cpp/BuildExtension/Executable/Main.cpp
+++ Samples/Cpp/BuildExtension/Executable/Main.cpp
@@ -7,5 +7,6 @@ int main()
 #else
 	std::cout << "Hello World..." << std::endl;
 #endif
+
 	return 0;
 }
---
+++ Samples/Cpp/BuildExtension/Tool/Main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "Tool, Soup Style!" << std::endl;
+
+	// pid_t processId = fork();
+	// if (processId == 0)
+	// {
+	// 	std::cout << "Child" << std::endl;
+	// }
+	// else
+	// {
+	// 	std::cout << "Parent" << std::endl;
+	// }
+
+	return 0;
+}
