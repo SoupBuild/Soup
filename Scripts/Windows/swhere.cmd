940003c986329021cbc6e325b75b71e6c20c7b1e
Matthew Asplund <mwasplund@outlook.com>
2024-12-10 04:29:24 -0600

    Fix windows paths

--- Scripts/Windows/swhere.cmd
+++ Scripts/Windows/swhere.cmd
@@ -3,5 +3,6 @@ SETLOCAL
 SET ScriptsDir=%~dp0
 SET RootDir=%ScriptsDir%..\..
 SET OutDir=%RootDir%\out
+SET MSBuildDir=%OutDir%\msbuild
 
-%OutDir%\bin\Swhere\Release\net8.0\win-x64\publish\swhere.exe %*
+%MSBuildDir%\bin\Swhere\Release\net8.0\win-x64\publish\swhere.exe %*
