ba501a0defaa39ec2836ad6bae83fd494593cab5
Matthew Asplund <mwasplund@outlook.com>
2024-12-19 18:38:25 -0600

    Update to .net 9

--- Scripts/Windows/build-view.cmd
+++ Scripts/Windows/build-view.cmd
@@ -10,6 +10,6 @@ REM - Cleanup old publish to work around bug in publish
 rmdir /S /Q %RootDir%\out\bin\SoupView\%Flavor%\net8.0\win-x64\publish\
 
 REM - Publish SWhere tool
-echo dotnet publish %SWhereDir% -c %Flavor% -f net8.0 -r win-x64 --self-contained true
-dotnet publish %SWhereDir% -c %Flavor% -f net8.0 -r win-x64 --self-contained true
+echo dotnet publish %SWhereDir% -c %Flavor% -f net9.0 -r win-x64 --self-contained true
+dotnet publish %SWhereDir% -c %Flavor% -f net9.0 -r win-x64 --self-contained true
 if %ERRORLEVEL% NEQ  0 exit /B %ERRORLEVEL%
