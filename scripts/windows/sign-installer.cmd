8fb152d8ec4c335b88b6b50e51d9f190737a4cb2
Matthew Asplund <mwasplund@outlook.com>
2025-01-02 14:14:30 -0600

    Fix sign scripts

--- scripts/windows/sign-installer.cmd
+++ scripts/windows/sign-installer.cmd
@@ -4,8 +4,10 @@ SET ScriptsDir=%~dp0
 SET RootDir=%ScriptsDir%..\..
 SET OutDir=%RootDir%\out
 
+SET SOUP_VERSION=0.41.3
+
 SET SIGN_COMMAND=signtool sign /n "Open Source Developer, Matthew Asplund" /t http://time.certum.pl /fd sha1 /v
 
-SET SignFiles=%OutDir%\soup-build-2-windows-x64.msi
+SET SignFiles=%OutDir%\release\soup-build-%SOUP_VERSION%-windows-x64.msi
 
 %SIGN_COMMAND% %SignFiles%
